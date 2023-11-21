0eaa53fd1920d29c841c0fb7f5f07822eb80d7ee
RishabhSharma7 <rishabhsharma1353@gmail.com>
2023-11-21 17:46:34 +0530

    code update

--- Routes/blogRoute.js
+++ Routes/blogRoute.js
@@ -4,5 +4,5 @@ const user=require("../Controllers/userController");
 const router=express.Router();
 router.route("/").get(blog.getAllBlogs).post(user.protect,blog.createblog);
 router.route("/search").get(blog.searchProduct);
-router.route("/:id").get(blog.getBlog).patch(user.protect,user.restrict('admin'),blog.updateBlog).delete(user.protect,user.restrict,blog.deleteBlog);
+router.route("/:id").get(blog.getBlog).patch(user.protect,user.restrict('admin'),blog.updateBlog).delete(user.protect,user.restrict('admin'),blog.deleteBlog);
 module.exports=router;
